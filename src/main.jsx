18dd899e1e0c69e39e21f430d684267de07abb50
Rizwanishere <rizwantopper@gmail.com>
2024-07-16 02:14:40 +0530

    Code Cleanup

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,8 @@
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom/client";
 import React from 'react'
 import App from './App';
 import './index.css';
 
-//rizwan
-// ReactDOM.render(<App/>,document.getElementById('root'));
-
-
-//faisal
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
